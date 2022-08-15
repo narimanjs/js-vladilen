d383a62760d0abc30c64ae18934fd4907377e825
Nariman <mirmanov1@mail.ru>
2022-08-16 00:15:52 +0300

    feat: done lesson 30

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
 
-//class -> .CLASS_NAME
-//id -> #ID
+// //class -> .CLASS_NAME
+// //id -> #ID
 
-const a = document.querySelector('a');
-const h1 = document.querySelector('h1');
-const input = document.querySelector('input');
-const p = document.querySelector('p');
-const oldHref = a.getAttribute('href');
+// const a = document.querySelector('a');
+// const h1 = document.querySelector('h1');
+// const input = document.querySelector('input');
+// const p = document.querySelector('p');
+// const oldHref = a.getAttribute('href');
+// //
+// const box1 = document.querySelector('#box1');
+// const box2 = document.querySelector('#box2');
+
+// h1.textContent = 'changed from JS';
+// a.setAttribute('href', 'https://ya.ru');
+// a.textContent = 'Yandex';
+// a.setAttribute('title', 'yaaaa');
+// p.innerHTML = '<h2 style="color: red;">from JS</h2>';
 
 
-h1.textContent = 'changed from JS';
-a.setAttribute('href', 'https://ya.ru');
-a.textContent = 'Yandex';
-a.setAttribute('title', 'yaaaa');
-p.innerHTML = '<h2 style="color: red;">from JS</h2>';
+// box1.classList.add('blue');
+// box2.classList.add('yellow');
+// box1.classList.remove('blue');
+// const hasClass = box2.classList.contains('blue');
+// console.log(hasClass);
 
-console.log(input.value);
-console.log(h1.textContent);
-console.log(a.attributes);
+// if (hasClass) {
+//     box1.classList.remove('blue');
+// } else {
+//     box1.classList.add('blue');
+// }
+// Добавление событий Урок 29
+
+const button = document.querySelector('button'),
+      h1 = document.querySelector('h1'),
+      input = document.querySelector('input');
+
+button.addEventListener('click', () => {
+    console.log('clicked!');
+    h1.textContent = input.value;
+});
+
+h1.addEventListener('mouseenter', function () {
+    this.style.color = 'red';
+    this.style.backgroundColor ='black';
+})
+h1.addEventListener('mouseleave', function () {
+    this.style.color = 'black';
+    this.style.backgroundColor ='transparent';
+
+    
+})
