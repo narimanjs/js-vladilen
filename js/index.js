2ae2f7b8ce54e7a070d7acdb9a70a50bb88c9d06
Nariman <mirmanov1@mail.ru>
2022-09-04 22:16:41 +0300

    feat: add lesson 44

--- js/index.js
+++ js/index.js
@@ -1,20 +1,62 @@
 let $start = document.querySelector('.btn')
 let $game = document.querySelector('.game')
+let $time = document.querySelector('#time')
+let $result = document.querySelector('#result')
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
 
 let score = 0;
+let isGameStarted = false;
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
 
 function startGame() {
-  $game.style.backgroundColor = '#fff'
+  score = 0
+  setGameTime()
+  $timeHeader.classList.remove('hide')
+  $resultHeader.classList.add('hide')
+  isGameStarted = true
+  $game.style.backgroundColor = '#ea85a9'
   $start.classList.add('hide')
+  
+
+  var interval = setInterval(function() {
+    // console.log('interval', $time.textContent);
+    let time = parseFloat($time.textContent)
+
+    if(time <= 0) {
+      //end game
+      clearInterval(interval)
+      endGame()
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1)
+    }
+  }, 100)
 
   renderBox()
 }
-
+function setGameScore() {
+  $result.textContent = score.toString()
+}
+function setGameTime() {
+  let time = 5
+  $time.textContent = time.toFixed(1)
+}
+function endGame() {
+  isGameStarted = false
+  setGameScore()
+  $start.classList.remove('hide')
+  $game.innerHTML = ''
+  $game.style.backgroundColor = '#ccc'
+  $timeHeader.classList.add('hide')
+  $resultHeader.classList.remove('hide')
+}
 function handleBoxClick(event) {
+  if(!isGameStarted){
+    return
+  }
   if (event.target.dataset.box) {
     score++
     renderBox()
@@ -24,15 +66,24 @@ function handleBoxClick(event) {
 function renderBox() {
   $game.innerHTML = ''
   let box = document.createElement('div')
+  let boxSize = getRandom(20, 90)
+  let gameSize = $game.getBoundingClientRect()
+  let maxTop = gameSize.height - boxSize
+  let maxLeft = gameSize.width - boxSize
 
-  box.style.height = box.style.width = '50px'
+  box.style.height = box.style.width = boxSize + 'px'
+  box.style.borderRadius = '50%'
   box.style.position = 'absolute'
   box.style.backgroundColor = '#000'
-  box.style.top = '50px'
-  box.style.left = '70px'
+  box.style.top = getRandom(0, maxTop) + 'px'
+  box.style.left = getRandom(0, maxLeft) + 'px'
   box.style.cursor = 'pointer'
   box.setAttribute('data-box', 'true')
 
   $game.insertAdjacentElement('afterbegin', box)
 
+}
+
+function getRandom (min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 }
